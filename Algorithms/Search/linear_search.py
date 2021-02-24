6fcb651a8d92e44aadd339c1ecbb990c20787095
Jothamsl <jothamsl1@gmail.com>
2021-02-24 08:58:44 +0100

    Fixed linear search bug

--- Algorithms/Search/linear_search.py
+++ Algorithms/Search/linear_search.py
@@ -10,19 +10,18 @@ def search(arr: List[int], x: int) -> int:
     """
     Search for value (x) in arr
     """
-    for _, i in enumerate(arr):  # Loop through array
-        if x == i:  # if value x is equal to i
-            return _  # return the index
+    for i in range(len(arr)):  # Loop through array
+        if x == arr[i]:  # if value x is equal to i
+            return i  # return the index
     return -1
 
 
 if __name__ == "__main__":
-    size = input("Input array size: ")
     array = list(map(int, input("Input array values: ").split(" ")))
     val = int(input("Input search value: "))
     res = search(array, val)
 
-    if (val != -1):
+    if (res != -1):
         print("Value {} found in index {}".format(val, res))
     else:
         print("Value not found")
